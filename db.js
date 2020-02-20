38014ac0b05bc5b3ea044ba739770f0bd883a38c
Chaise Matev <aj_fl81@msn.com>
2020-02-19 20:20:54 -0500

    created tables

--- db.js
+++ db.js
@@ -1,22 +1,48 @@
 const pg = require('pg');
+const uuid = require('uuid');
+const { Client } = pg;
 
-const sync = async()=> {
+const client = new Client('postgres://localhost/user_department_db');
+
+client.connect();
+
+//CRUD
+const sync = async () => {
+  const SQL = `
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS departments;
+
+    CREATE TABLE departments(
+      id UUID PRIMARY KEY,
+      name VARCHAR
+    );
+
+    CREATE TABLE users(
+      id UUID PRIMARY KEY,
+      name VARCHAR,
+      "departmentId" UUID REFERENCES departments(id)
+    );
+
+    INSERT INTO departments (id, name) VALUES (uuid(), 'Chaise');
+    INSERT INTO departments (id, name) VALUES (uuid(), 'Kelly');
+  `;
+  await client.query(SQL);
   //DROP and RECREATE TABLES
   //remember "departmentId" will need to be in quotes
 };
 
-const readDepartments = async()=> {
+const readDepartments = async () => {
   return [];
 };
 
-const readUsers = async()=> {
+const readUsers = async () => {
   return [];
 };
 
 module.exports = {
   sync,
   readDepartments,
-  readUsers
+  readUsers,
 };
 //you will eventually need to export all of these
 /*
@@ -32,4 +58,3 @@ module.exports = {
   updateDepartment
 };
 */
-
